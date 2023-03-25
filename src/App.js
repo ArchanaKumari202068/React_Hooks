751c1bac3dd89a924888f76b9a240b42af2522ae
Archana Sharma <archanakumari202068@gmail.com>
2023-03-25 11:34:13 +0530

    How to Toggle the Data onclick using Hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React ,{useState}from 'react'
 import './App.css';
+const App = () => {
 
-function App() {
+  // var val = 'Heelloooo';
+  // const changeName = ()=>{
+  //   val = "Welcome"
+
+ const [myName,setmyName] =useState("Hello Archana.!");
+  const changeName = ()=>{
+
+    // let val = myName;
+    // if (val==='Hello Archana.!'){
+    //   setmyName('Welcome.!')
+    // } else{
+    //   setmyName('Hello Archana.!')
+    // }
+
+    let val = myName;
+
+    //Ternary operator
+
+     (val==='Hello Archana.!') ?
+      setmyName('Welcome.!'):setmyName('Hello Archana.!');
+    }
+
+    // to change after click on button welcome show but if again click the we want to go back
+    //How to Toggle the Data onclick using Hook
+    // setmyName('welcome.!');
+
+  
+  
+    console.log(myName);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* JSx{  <h1>{5+5}</h1> } */}
+      <h1>{myName}</h1>
+      <button className="btn" onClick={changeName} >click me </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
