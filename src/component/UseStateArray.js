7a0f1d7cb26468328fdf9460dcf773285885a0af
Archana Sharma <archanakumari202068@gmail.com>
2023-03-26 17:59:02 +0530

    remove element by id

--- src/component/UseStateArray.js
+++ src/component/UseStateArray.js
@@ -7,7 +7,7 @@ const UseStateArray = () => {
         },{
             id:1,myName: "Madhu Priya", age : 18
         }, {
-            id :2, myName: "Dimpi",age:27
+            id :2, myName: "Dimpi",age:22
         }
 
     ]
@@ -15,18 +15,34 @@ const UseStateArray = () => {
 
     const clearArray=()=>{
         setmyArray([]);
-        console.log("check")
 
-    }  
+    }
+    
+    const removeElem =(id)=> {
+        const myNewArray =myArray.filter((curElm)=>{
+            return  curElm.id!==id;
+        })
+        setmyArray(myNewArray);
+        alert(id);
+
+    }
+
     return (
-    <>
+        <>
         {
-            myArray.map((curElm) => { return <h1 className='h1style' key = {curElm.id}> Name: {curElm.myName} & Age :{curElm.age}</h1>})
+            myArray.map((curElm) => { 
+                return (
+                    <h1 className='h1style' key = {curElm.id}> Name:
+                        {curElm.myName} & Age :{curElm.age}
+                        <button className='btnInner' onClick={ ()=>removeElem(curElm.id)}>remove</button>
+                    </h1>
+                );
+            })
         }
         <button className="btn" onClick={clearArray}>Clear</button>
 
         {/* <h1 className='h1style'>Name:Archana Bhardwaj & Age :20</h1> */}
-    </>
+        </>
   )
 }
 
